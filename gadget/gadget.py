e42521b37fad80433f335c71fa4b5ef4a20fee6e
alumilu <hubert.lu@gmail.com>
2020-04-19 20:11:34 +0800

    modify usb port power control gadget

--- gadget/gadget.py
+++ gadget/gadget.py
@@ -1,13 +1,19 @@
+import subprocess
 
 class FacePlayGadget(object):
-    
+    def __init__(self):
+        subprocess.run(['sudo', 'uhubctl', '-l', '2-1', '-p', '1', '-a', 'off']) #turn off the usb port by by default
+
+    def __del__(self):
+        return
+
     def play(self, **kwargs):
         
         if kwargs.get("face_detected") is not None:
             if kwargs.get("face_detected") == True:
-                print (">>>>>>>>>>>Kid is watching  TV! Turn it off<<<<<<<<<<<<<<<<")
-                print (kwargs.get("detected_time"))
-                print (kwargs.get("the_face"))
+                print ('%s is detected, time: %s' % (kwargs.get("the_face"), kwargs.get("detected_time")))
+                subprocess.run(['sudo', 'uhubctl', '-l', '2-1', '-p', '1', '-a', 'on']) #turn off the usb port
             else:
-                print (">>>>>>>>>>>no kid found<<<<<<<<<<<<<")
+                print ('no faces are detected...')
+                subprocess.run(['sudo', 'uhubctl', '-l', '2-1', '-p', '1', '-a', 'off']) #turn off the usb port
         
